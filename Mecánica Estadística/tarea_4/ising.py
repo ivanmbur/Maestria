d7c914ea594c14e33fe939e020eb1b4114d86d77
Iván <ivan@Ivans-MacBook-Pro.local>
2018-03-27 07:06:20 -0500

    se adiciona el .gitignore

--- Mecánica Estadística/tarea_4/ising.py
+++ Mecánica Estadística/tarea_4/ising.py
@@ -3,25 +3,16 @@ import random
 import matplotlib.pyplot as plt
 from matplotlib import rc
 
-#Se definen los parametros de la simulacion
+#Se definen los parametros de la simulación
 
 L_c = 0.5*np.arcsinh(1)
-temperaturas = np.linspace(0.5, 3.2, 1000)
+temperaturas = np.linspace(0.5, 3.5, 1000)
 N = 10
-nsteps = 1000000
+nsteps = 100000
 nsteps_equilibrio = 10000
+energy_graphs = [0,200,500,700]
 
-#Se crea una funcion que calcula la energia de un reticulo en unidades de J
-
-def H(sigma):
-	E = 0
-	for i in range(0, len(sigma)):
-		for j in range(0, len(sigma[i])):
-			E += sigma[i,j]*(sigma[i, (j+1) % len(sigma[i])] + sigma[i, (j-1) % len(sigma[i])] + sigma[(i+1) % len(sigma), j] + sigma[(i-1) % len(sigma), j])
-	E = -0.5*E
-	return E
-
-#Se crean las listas de energia y magnetizacion promedio
+#Se crean las listas de energía y magnetización promedio
 
 E_promedio = np.zeros(len(temperaturas))
 M_promedio = np.zeros(len(temperaturas))
@@ -30,36 +21,41 @@ M_promedio = np.zeros(len(temperaturas))
 
 for t in range(0,len(temperaturas)):
 
+	print("Temperatura %d de %d" % (t,len(temperaturas)))
+
 	L = 1.0/temperaturas[t]
 
-	#Se crea el reticulo de espines 
+	#Se crea el retículo de espines 
 
 	reticulo = np.zeros((N, N))
 
-	#Se crea una lista de energias y magnetizaciones
+	#Se crea una lista de energías y magnetizaciones
 
 	E = np.zeros(nsteps)
 	M = np.zeros(nsteps)
 
-	#Se le asigna un valor aleatorio en {-1,1} a cada sitio en el reticulo
+	#Se le asigna un valor aleatorio en {-1,1} a cada sitio en el retículo
 
 	for i in range(0, N):
 		for j in range(0,N):
 			reticulo[i,j] = random.randrange(-1,2,2)
 
-	#Se inicializa la lista de energias y magnetizaciones
-	E[0] = H(reticulo)
+	#Se inicializan las listas de energias y magnetizaciones
+	E[0] = 0
+	for i in range(0, len(reticulo)):
+		for j in range(0, len(reticulo[i])):
+			E[0] += reticulo[i,j]*(reticulo[i, (j+1) % len(reticulo[i])] + reticulo[i, (j-1) % len(reticulo[i])] + reticulo[(i+1) % len(reticulo), j] + reticulo[(i-1) % len(reticulo), j])
+	E[0] = -0.5*E[0]
 	M[0] = np.sum(reticulo)
 
 	for n in range(1, nsteps):
 
-
-		#Se selecciona una entrada aleatoria en el reticulo
+		#Se selecciona una entrada aleatoria en el retículo
 
 		i = random.randrange(0, len(reticulo))
 		j = random.randrange(0, len(reticulo[i]))
 
-		#Se calcula el cambio en la energia
+		#Se calcula el cambio en la energía
 
 		Delta_E =2*reticulo[i,j]*(reticulo[i, (j+1) % len(reticulo[i])] + reticulo[i, (j-1) % len(reticulo[i])] + reticulo[(i+1) % len(reticulo), j] + reticulo[(i-1) % len(reticulo), j]) 
 
@@ -67,28 +63,39 @@ for t in range(0,len(temperaturas)):
 
 		if (Delta_E <= 0) or (random.uniform(0, 1) < np.exp(-L*Delta_E)):
 			reticulo[i,j] = -reticulo[i,j]
+			E[n] = E[n-1] + Delta_E
+			M[n] = M[n-1] + 2*reticulo[i,j]
+		else:
+			E[n] = E[n-1]
+			M[n] = M[n-1]
 		
-		#Se calcula la nueva energia
-		E[n] = H(reticulo)
-		M[n] = np.sum(reticulo)
-		
 
-	#Se hacen graficas de la evolucion de la energia
+	#Se hacen graficas de la evolución de la energía
 
-	fig, ax = plt.subplots()
-	ax.plot(range(0, nsteps), E/(N*N))
-	ax.set_xlabel("Iteración")
-	ax.set_ylabel("Energía por sitio ($J$)")
-	ax.set_title("$T=%.2f J/K_B$, $N=%d$" % (temperaturas[t], N*N))
-	fig.savefig("energia_%f.png" % temperaturas[t])
-	plt.close(fig) 
+	if t in energy_graphs:	
 
-	#Se guarda el valor de la energia promedio y la magnetizacion promedio
+		fig, ax = plt.subplots()
+		ax.plot(range(0, nsteps), E/(N*N))
+		ax.set_xlabel("Iteración")
+		ax.set_ylabel("Energía por sitio ($J$)")
+		ax.set_title("$T=%.2f J/K_B$, $N=%d$" % (temperaturas[t], N*N))
+		fig.savefig("%.2f_energia.png" % temperaturas[t])
+		plt.close(fig) 
+
+	#Se guarda el valor de la energía promedio y la magnetización promedio
 
 	E_promedio[t] = np.mean(E[nsteps_equilibrio:])
 	M_promedio[t] = np.mean(M[nsteps_equilibrio:])
 
-#Se grafica la magnetizacion
+#Se grafica la magnetización y la energía promedio
+
+fig, ax = plt.subplots()
+ax.scatter(temperaturas, E_promedio/(N*N), alpha = 0.3)
+ax.set_xlabel("Temperatura ($J/K_B$)")
+ax.set_ylabel("Energía por sitio ($J$)")
+ax.set_title("$N=%d$, $%d$ pasos con $%d$ para estabilizar" % (N*N, nsteps, nsteps_equilibrio))
+fig.savefig("energia.png")
+plt.close(fig)
 
 fig, ax = plt.subplots()
 ax.scatter(temperaturas, M_promedio/(N*N), alpha = 0.3)
