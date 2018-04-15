14ea9740bbc856e0558e1ab6366bd2e270440a43
Iván Mauricio Burbano Aldana <ivanmbur@gmail.com>
2018-04-15 15:15:28 -0500

    se completa la tarea 4 de electrodinamica y se termina la tarea 5 de mecanica estadistica salvo por el ultimo punto

---
+++ Mecánica Estadística/tarea_5/calculos.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+K = np.log((3-np.sqrt(2))/(np.sqrt(2)-1))/4
+print("K = %f" % K)
+d1R1 = 1+4*K*((((3.0*np.exp(4.0*K)-1)*(np.exp(4.0*K)+3.0))/((np.exp(4.0*K)+3.0)*(np.exp(4.0*K)+3.0)))-((3.0*np.exp(4.0*K)-3.0)/((np.exp(4.0*K)+3.0)*np.sqrt(2.0))))/np.sqrt(2.0)
+print("d1R1 = %f" % d1R1)
+y_T = np.log(d1R1)/np.log(np.sqrt(3)) 
+print("y_T = %f" % y_T) 
+d2R2 = 3.0/np.sqrt(2)
+print("d2R2 = %f" % d2R2)
+y_h = np.log(d2R2)/np.log(np.sqrt(3))
+print("y_h = %f" % y_h)
+nu = 1/y_T
+print("nu = %f" % nu) 
