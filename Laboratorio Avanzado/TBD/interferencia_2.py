7201f25832935b87949a2abc212fbc54c7f89ddc
Iván Mauricio Burbano Aldana <ivanmbur@gmail.com>
2018-10-30 05:54:42 -0500

    patrón de interferencia

---
+++ Laboratorio Avanzado/TBD/interferencia_2.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+plt.rc("text", usetex = True)
+plt.rc("text.latex", preamble = r"\usepackage[utf8]{inputenc} \usepackage{siunitx} \usepackage[version = 4]{mhchem} \usepackage{physics}")
+plt.rc("font", **{"family": "serif", "serif": ["Computer Modern"]})
+
+def I(angle, offset, I_0, w, q):
+	theta = angle-offset
+	return I_0*(1 + np.exp(-2*theta*theta/w)*np.cos(2*q*theta + np.pi))/2
+
+data = np.genfromtxt("interferencia.txt", usecols = (0, 1), encoding = "latin-1")
+
+angles = np.linspace(np.min(data[:,0]),np.max(data[:,0]),1000)
+popt, pcov = curve_fit(I, data[:,0], data[:,1], p0 = [0.53, np.max(data[:,1]), 8, 8.7])
+
+fig, ax = plt.subplots()
+ax.scatter(data[:,0], data[:,1], c = "k", alpha = 0.3)
+ax.plot(angles, I(angles, *popt), c = "k")
+ax.set_xlabel(r"$\theta(\si{\degree})$")
+ax.set_ylabel(r"$I(\si{\micro\watt})$")
+ax.set_title(r"Patr\'on de Interferencia")
+fig.savefig("interferencia_2.png")
+plt.close(fig)
