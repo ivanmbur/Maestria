76e89d1e3c0460da8c123948aaba76268fef63f0
Iván Mauricio Burbano Aldana <ivanmbur@gmail.com>
2018-11-07 18:58:18 -0500

    se limpian datos y se hacen las graficas de la actividad 2.2

--- Laboratorio Avanzado/Efecto_Hall/actividad_2.py
+++ Laboratorio Avanzado/Efecto_Hall/actividad_2.py
@@ -13,5 +13,22 @@ print(data.shape)
 B = [[-ufloat(fit[0],1)*ufloat(I_B,0.01) - ufloat(fit[1],1) for I_B in data[i][:,0]] for i in range(0,len(data))]
 B_nom = np.array([np.array([b.nominal_value for b in B[i]]) for i in range(0,len(B))])
 
-fit = np.array([np.polyfit(B_nom[i], data[i][:,1], 1) for i in range(0,len(B))])
-sigma_V = sigma_B
+R = np.array([])
+
+for i in range(0,len(B_nom)):
+	print("Se tiene una resistencia de Hall de %f con una incertidumbre de %f para la corriente transversal %f" % (R[i], sigma_R[i], I_p[i]))
+
+fit = np.array([np.polyfit(B_nom[i], data[i][:,1], 1) for i in range(0,len(B_nom))])
+sigma_V = np.array([np.sqrt(np.sum((data[i][:,1]-fit[i][1]-fit[i][0]*B_nom[i])**2)/(len(B_nom[i])-2)) for i in range(0,len(B_nom))])
+Delta = np.array([len(B_nom[i])*np.sum(B_nom[i]**2)-np.sum(B_nom[i])**2 for i in range(0,len(B_nom))])
+sigma_pendiente = np.array([sigma_V[i]*np.sqrt(len(B_nom[i])/Delta[i]) for i in range(0,len(B_nom))])
+sigma_intercepto = np.array([sigma_V[i]*np.sqrt(np.sum(B_nom[i]**2)/Delta[i]) for i in range(0,len(B_nom))])
+
+colors = ["b", "r", "g", "y", "m"]
+fig, ax = plt.subplots()
+for i in range(0,len(B_nom)):
+	ax.errorbar(B_nom[i], data[i][:,1], xerr = [b.std_dev for b in B[i]], yerr = 0.1, fmt = "o", alpha = 0.3, c = colors[i], label = r"$I_p = \SI{%f}{\milli\A}$" % I_p[i])
+	ax.plot(B_nom[i], fit[i][0]*B_nom[i]+fit[i][1], c = colors[i], alpha = 0.3)
+ax.legend()
+fig.savefig("Resistencias_Hall.png")
+plt.close(fig) 
